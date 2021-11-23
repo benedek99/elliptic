4d1976e21d5d2eb62a9ad65ab01c55edb3bfad95
benedek99 <szabobenedek99@gmail.com>
2021-11-23 10:09:41 +0100

    imported packages

--- basics.py
+++ basics.py
@@ -1,15 +1,3 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
 import collections
 
 
@@ -219,5 +207,3 @@ if __name__ == "__main__":
     assert ecdh.secret(bpriv, apub) != ecdh.secret(bpriv, cpub)
     assert ecdh.secret(cpriv, bpub) != ecdh.secret(cpriv, apub)
     pass
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
